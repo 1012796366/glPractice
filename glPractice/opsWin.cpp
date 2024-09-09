afff0fa7d404b107926a57be72b5bbc742f69969
1i9h7_b1u3 <1012796366@qq.com>
2024-09-09 12:29:34 +0800

    窗口操作函数

--- glPractice/opsWin.cpp
+++ glPractice/opsWin.cpp
@@ -3,5 +3,128 @@
 void setBackgroundColor(GLfloat red, GLfloat green, GLfloat blue, GLfloat alpha)
 {
 	glClearColor(red, green, blue, alpha);
-	glClear(GL_COLOR_BUFFER_BIT);
+}
+
+void clearBackground()
+{
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+}
+
+void swapBuffer(basicWindow* basicWindowObj)
+{
+	glfwSwapBuffers(basicWindowObj->window);
+}
+
+void pollEvents()
+{
+	glfwPollEvents();
+}
+
+void shutWindow(basicWindow* opsObject)
+{
+	glfwTerminate();
+	opsObject->initFlag = false;
+}
+
+void initWindow(basicWindow* opsObject, char const* title, int scrFlag, int width, int height,
+	void (*size_callback)(GLFWwindow*, int, int))
+{
+	if (opsObject->initFlag)
+	{
+		// a basicWindow object should only manage 1 window at a time
+		// will throw an unreadyWindowException with default exception message
+		throw unreadyWindowException();
+	}
+	// GLFW Initialization
+	try
+	{
+		initGLFW(opsObject, title, width, height, scrFlag);
+	}
+	catch (const std::runtime_error& glfwException)
+	{
+		throw glfwException;
+	}
+	// End of GLFW Initialization
+	// GLAD Initialization
+	try
+	{
+		initGLAD();
+	}
+	catch (const std::runtime_error& gladException)
+	{
+		throw gladException;
+	}
+	// End of GLAD Initialization
+
+	// Register frame buffer size callback function
+	// Will use default one if user doesn't specify any
+	if (size_callback == nullptr) 
+	{
+		size_callback = framebuffer_size_callback;
+	}
+	glfwSetFramebufferSizeCallback(opsObject->window, size_callback);
+	opsObject->initFlag = true;
+}
+
+void initGLFW(basicWindow* opsObject, char const* title, int width, int height, int scrFlag)
+{
+	glfwInit();
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+	glfwWindowHint(GLFW_RESIZABLE, false);
+
+	auto monitor = glfwGetPrimaryMonitor();
+	if (monitor == nullptr)
+	{
+		shutWindow(opsObject);
+		throw std::runtime_error("Failed to get primary monitor.");
+	}
+	GLFWvidmode const* vidMode = glfwGetVideoMode(monitor);
+	if (vidMode == nullptr)
+	{
+		shutWindow(opsObject);
+		throw std::runtime_error("Failed to get video mode.");
+	}
+
+	switch (scrFlag)
+	{
+		// Fullscreen
+	case GLBW_SCREEN_FULLSCREEN:
+		opsObject->window = glfwCreateWindow(width, height, title, monitor, nullptr);
+		break;
+		// Fullscreen Borderless
+	case GLBW_SCREEN_FULLBORDER:
+		glfwWindowHint(GLFW_DECORATED, false);
+		opsObject->window = glfwCreateWindow(vidMode->width, vidMode->height, title, nullptr, nullptr);
+		break;
+		// Windowed Borderless
+	case GLBW_SCREEN_BORDERLESS:
+		glfwWindowHint(GLFW_DECORATED, false);
+		opsObject->window = glfwCreateWindow(width, height, title, nullptr, nullptr);
+		break;
+		// Windowed
+	case GLBW_SCREEN_WINDOWED:
+		opsObject->window = glfwCreateWindow(width, height, title, nullptr, nullptr);
+		break;
+	}
+	if (opsObject->window == nullptr)
+	{
+		shutWindow(opsObject);
+		throw std::runtime_error("Failed to init GLFW window.");
+	}
+	glfwMakeContextCurrent(opsObject->window);
+}
+
+void initGLAD()
+{
+	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+	{
+		throw std::runtime_error("Failed to init GLAD.");
+	}
+}
+
+bool isWindowAlive(basicWindow* opsObject)
+{
+	return !glfwWindowShouldClose(opsObject->window);
 }
--- glPractice/opsWin.h
+++ glPractice/opsWin.h
@@ -1,4 +1,37 @@
 #pragma once
 #include "initWin.h"
 
-void setBackgroundColor(basicWindow *opsObject, GLfloat red, GLfloat green, GLfloat blue, GLfloat alpha);
+// set the background color without flush the window
+void setBackgroundColor(GLfloat red, GLfloat green, GLfloat blue, GLfloat alpha=1.0f);
+
+// clear the background
+void clearBackground();
+
+// will call glfwSwapBuffers with this->window passed as argument
+void swapBuffer(basicWindow* basicWindowObj);
+
+// will call glfwPollEvents
+void pollEvents();
+
+// OpenGL Window Initializer
+// size_callback will use default function when it's set to nullptr or NULL
+// window will appear as this function successfully initialized
+// value for scrFlag can be found in preFlag.h
+void initWindow(basicWindow* opsObject, char const* title, int scrFlag = GLBW_SCREEN_WINDOWED, int width = 800, int height = 600,
+	void (*size_callback)(GLFWwindow*, int, int) = nullptr);
+
+// call initWindow instead
+void initGLFW(basicWindow* opsObject, char const* title, int width, int height, int scrFlag);
+
+
+// call initWindow instead
+void initGLAD();
+
+// return FALSE as the window should CLOSE
+// return TRUE as the window should NOT CLOSE
+bool isWindowAlive(basicWindow* opsObject);
+
+// will call glfwTerminate
+// will not be called automatically when basicWindow object is destroyed
+// need to manually call glfwTerminate if no basicWindow object is still alive
+void shutWindow(basicWindow* opsObject);
