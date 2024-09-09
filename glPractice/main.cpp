782fc5830519f310cf33e9047514732337a16dc4
1i9h7_b1u3 <1012796366@qq.com>
2024-09-09 12:29:24 +0800

    主函数

--- glPractice/main.cpp
+++ glPractice/main.cpp
@@ -1,24 +1,131 @@
 #include "initWin.h"
 #include <iostream>
 #include "preFlag.h"
+#include "opsWin.h"
+#include "inputProc.h"
+#include "shader.h"
+#include "absNode.h"
 
-// inputProc.cpp
-void processInput(GLFWwindow *window);
+int screenWidth = 800;
+int screenHeight = 600;
 
 int main()
 {
-	basicWindow base;
-	base.initWindow("LearnOpenGL", GLBW_SCREEN_FULLBORDER);
-	base.setBackgroundColor(0.2f, 0.3f, 0.3f);
-	
-	while (base.isWindowAlive())
-	{
-		processInput(base.window);
-		base.clearBackgroundColor();
+	GLfloat vertices[] = {
+		-0.5f, -0.5f, -0.5f,  0.0f, 0.0f,		// 0
+		 0.5f, -0.5f, -0.5f,  1.0f, 0.0f,		// 1
+		 0.5f,  0.5f, -0.5f,  1.0f, 1.0f,		// 2
+		 0.5f,  0.5f, -0.5f,  1.0f, 1.0f,		// 2
+		-0.5f,  0.5f, -0.5f,  0.0f, 1.0f,		// 3
+		-0.5f, -0.5f, -0.5f,  0.0f, 0.0f,		// 0
 
-		base.swapBuffer();
-		base.pollEvents();
+		-0.5f, -0.5f,  0.5f,  0.0f, 0.0f,		// 4
+		 0.5f, -0.5f,  0.5f,  1.0f, 0.0f,		// 5
+		 0.5f,  0.5f,  0.5f,  1.0f, 1.0f,		// 6
+		 0.5f,  0.5f,  0.5f,  1.0f, 1.0f,		// 6
+		-0.5f,  0.5f,  0.5f,  0.0f, 1.0f,		// 7
+		-0.5f, -0.5f,  0.5f,  0.0f, 0.0f,		// 4
+
+		-0.5f,  0.5f,  0.5f,  1.0f, 0.0f,		// 8
+		-0.5f,  0.5f, -0.5f,  1.0f, 1.0f,		// 9
+		-0.5f, -0.5f, -0.5f,  0.0f, 1.0f,		// 10
+		-0.5f, -0.5f, -0.5f,  0.0f, 1.0f,		// 10
+		-0.5f, -0.5f,  0.5f,  0.0f, 0.0f,		// 4
+		-0.5f,  0.5f,  0.5f,  1.0f, 0.0f,		// 8
+
+		 0.5f,  0.5f,  0.5f,  1.0f, 0.0f,		// 11
+		 0.5f,  0.5f, -0.5f,  1.0f, 1.0f,		// 2
+		 0.5f, -0.5f, -0.5f,  0.0f, 1.0f,		// 12
+		 0.5f, -0.5f, -0.5f,  0.0f, 1.0f,		// 12
+		 0.5f, -0.5f,  0.5f,  0.0f, 0.0f,		// 13
+		 0.5f,  0.5f,  0.5f,  1.0f, 0.0f,		// 11
+
+		-0.5f, -0.5f, -0.5f,  0.0f, 1.0f,		// 10
+		 0.5f, -0.5f, -0.5f,  1.0f, 1.0f,		// 14
+		 0.5f, -0.5f,  0.5f,  1.0f, 0.0f,		// 15
+		 0.5f, -0.5f,  0.5f,  1.0f, 0.0f,		// 15
+		-0.5f, -0.5f,  0.5f,  0.0f, 0.0f,		// 4
+		-0.5f, -0.5f, -0.5f,  0.0f, 1.0f,		// 10
+
+		-0.5f,  0.5f, -0.5f,  0.0f, 1.0f,		// 3
+		 0.5f,  0.5f, -0.5f,  1.0f, 1.0f,		// 2
+		 0.5f,  0.5f,  0.5f,  1.0f, 0.0f,		// 11
+		 0.5f,  0.5f,  0.5f,  1.0f, 0.0f,		// 11
+		-0.5f,  0.5f,  0.5f,  0.0f, 0.0f,		// 16
+		-0.5f,  0.5f, -0.5f,  0.0f, 1.0f		// 3
+	};
+
+	basicWindow base;
+	try
+	{
+		initWindow(&base, "LearnOpenGL", GLBW_SCREEN_WINDOWED, screenWidth, screenHeight);
 	}
-	base.shutWindow();
+	catch (std::exception e)
+	{
+		std::cerr << e.what();
+		return 1;
+	}
+
+	primitive box;
+	//primitive face;
+	box.vaoID = genVAO();
+	box.vboID = genVBO();
+	box.eboID = genEBO();
+	box.shaderID = loadShaderFromFile("./Shader/vertex.vs", "./Shader/fragment.fs");
+
+	//face.vaoID = genVAO();
+	//face.vboID = genVBO();
+	//face.eboID = genEBO();
+	//face.shaderID = loadShaderFromFile("./Shader/vertex.vs", "./Shader/fragment.fs");
+
+	// ↓ 导入顶点数据与序列数据 ↓
+	buffVerticles(&box, vertices, sizeof(vertices));
+	//buffIndices(&box, indices, sizeof(indices));
+	//buffVerticles(&face, vertices, sizeof(vertices));
+	//buffIndices(&face, indices, sizeof(indices));
+	// ↑ 导入顶点数据与序列数据 ↑
+
+	addTexture(&box, loadTextureFromFile("./pic/container.jpg"));
+	addTexture(&box, loadTextureFromFile("./pic/awesomeface.png"));
+	//addTexture(&face, loadTextureFromFile("./pic/container.jpg"));
+	//addTexture(&face, loadTextureFromFile("./pic/awesomeface.png"));
+
+	setBackgroundColor(0.2f, 0.3f, 0.3f);
+
+	setInt(box.shaderID, "cusTex1", 0);
+	setInt(box.shaderID, "cusTex2", 1);
+	//setInt(face.shaderID, "cusTex1", 0);
+	//setInt(face.shaderID, "cusTex2", 1);
+	glEnable(GL_DEPTH_TEST);
+	while (isWindowAlive(&base))
+	{
+		processInput(base);
+		clearBackground();
+
+		translatePrimitive(&box, glm::vec3(0.4f, 0.3f, 0.2f));
+		rotatePrimitive(&box, (float)glfwGetTime(), glm::vec3(1.0f, 0.3f, 0.5f), "deg");
+		//scalePrimitive(&box, glm::vec3(0.5, 0.5, 0.5));
+
+		//translatePrimitive(&face, glm::vec3(-0.4f, -0.2f, 0.1f));
+		//rotatePrimitive(&face, 45.0f, glm::vec3(0.7f, 0.1f, -1.0f), "deg");
+		//scalePrimitive(&face, glm::vec3(0.5, 0.5, 0.5));
+
+		drawPrimitive(&box);
+		//drawPrimitive(&face);
+		
+		box.transform = glm::mat4(1.0f);
+		//face.transform = glm::mat4(1.0f);
+
+		
+		auto errID = glGetError();
+		if (errID)
+		{
+			std::cout << "Error occured with ID: " << errID << std::endl;
+		}
+
+		swapBuffer(&base);
+		pollEvents();
+	}
+	shutWindow(&base);
 	return 0;
 }
